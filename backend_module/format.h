5bf5dd81bcc60f6dcc2f5ba15a27521dc0334e16
Jerry <teamblackextra@gmail.com>
2025-07-24 18:42:49 +0700

    Write Simple Back end module function for init file if dont have or dont correct environment

--- backend_module/format.h
+++ backend_module/format.h
@@ -1,17 +1,19 @@
 #include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #define SUBJECT_NAME_SIZE 64
 #define SUBJECT_ID_SIZE 8
 
 typedef unsigned char bype;
 
-typedef struct{
+typedef struct _Subject_Payload{
     char SUBJECT_ID[SUBJECT_ID_SIZE];                // ID of subject ex: IT3100
     char SUBJECT_NAME[SUBJECT_NAME_SIZE];            // Name of Subject
-    bype term;                                       // Term recomment of Subject
+    bype term;                                       // Term recomment of Subject *** using 0 for make head empty ***
     bype credit_hour;                                // Credit hour of Subject
     bype ratio;                                      // Ratio calculate
-    Subject_Payload* link;                           // Link to another subject with same type
+    struct _Subject_Payload* link;                   // Link to another subject with same type
 }Subject_Payload;
 
 typedef struct{
@@ -19,6 +21,58 @@ typedef struct{
     int16_t curroffset;
     int16_t capacity;
 }Subject_Pooling;
+
+Subject_Pooling subject_pooling = {0};
+
+int IsPoolEmpty(){
+    return subject_pooling.curroffset == 0;
+}
+
+void Init_Pool(int16_t size){
+    subject_pooling.buffer = (Subject_Payload*) malloc(size * sizeof(Subject_Payload));
+    subject_pooling.capacity = size;
+    subject_pooling.curroffset = 0;
+}
+
+Subject_Payload* Allocate_Pool(const Subject_Payload src){
+    if(subject_pooling.curroffset >= subject_pooling.capacity){
+        printf("[Alert] Buffer Pooling Subject Payload is full\n");
+        return NULL;
+    }
+    subject_pooling.buffer[subject_pooling.curroffset++] = src;
+    return &subject_pooling.buffer[subject_pooling.curroffset-1];
+}
+
+int Pop_Pool(Subject_Payload* ret){
+    if(IsPoolEmpty()){
+        printf("[Alert] Buffer Pooling is Empty\n");
+        return 0;
+    }
+
+    *ret = subject_pooling.buffer[subject_pooling.curroffset--];
+    return 1;
+}
+
+int Detroy_Pool(){
+    if(subject_pooling.buffer == NULL){
+        printf("[Alert] Pooling have not been allocate allocate\n");
+        return 0;
+    }
+
+    free(subject_pooling.buffer);
+    subject_pooling.buffer = NULL;
+    subject_pooling.capacity = subject_pooling.curroffset = 0;
+    return 1;
+}
+
+int Clear_Pool(){
+    if(IsPoolEmpty()){
+        printf("[Alert] Buffer Pooling is Empty\n");
+        return 0;
+    }
+
+    subject_pooling.curroffset = 0;
+}
 
 typedef struct{
     char Subject_Type_Name[SUBJECT_NAME_SIZE];       // Name of Subject Type
--- backend_module/init.h
+++ backend_module/init.h
@@ -1,6 +1,4 @@
-#include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 #include "format.h"
 
 // check if dont have standard file it's will write it again
@@ -17,18 +15,44 @@ void Write_Environment(){
     // standard data date 7/20/2025 referent in table of subject of hanoi university of science and technology,
     // major : Computer Science (IT1)
     // gen : 68 (2023)
+
+    // type define
+    typedef struct{
+        int x,y,z;
+    }pair;
+
+    // Open File For Write
+    #ifdef _WIN32
+        #include <direct.h>
+        _mkdir("file");
+        _mkdir("file\\standardFile");
+    #else
+        #include <sys/stat.h>
+        mkdir("file", 0777);
+        mkdir("file/standardFile", 0777);
+    #endif
+    FILE* file = fopen("file/standardFile/standard_data.edbj", "wb");
+    if (!file) {
+        perror("[Error] Failed to create file");
+        return;
+    }
+
+    // Init Main Data Storate
     Data_Payload data = (Data_Payload){.capacity_type_subject = sizeSubjectType, .list_type_subject = NULL};
-    Subject_Type_Payload list[data.capacity_type_subject] = {0};
+    Subject_Type_Payload list[data.capacity_type_subject];
+    for(int i = 0; i < data.capacity_type_subject; i++) list[i].head.term = 0;
     data.list_type_subject = list;
+
+    // Init Name of Type Subject
     for(int i = co_so_nganh; i < sizeSubjectType; i++){
         strncpy(list[i].Subject_Type_Name,name_SJ_t[i],SUBJECT_NAME_SIZE);
     }
 
+    // Init Pooling
+    Init_Pool(256);
 
-    typedef struct{
-        int x,y,z;
-    }pair;
     {
+        // co so nganh
         pair arr[] = {
             {1,3,rt_50},
             {2,2,rt_60},
@@ -93,5 +117,70 @@ void Write_Environment(){
             "IT4653"
         };
         int size = sizeof(arr) / sizeof(arr[0]);
+
+        Subject_Payload *curr = NULL, *temp = NULL;
+        temp = Allocate_Pool(
+            (Subject_Payload){
+                .term = arr[0].x,
+                .credit_hour = arr[0].y,
+                .ratio = arr[0].z,
+                .link = NULL
+            }
+        );
+        strncpy(temp->SUBJECT_ID,ID[0],SUBJECT_ID_SIZE);
+        strncpy(temp->SUBJECT_NAME,name[0],SUBJECT_NAME_SIZE);
+        list[co_so_nganh].head = *temp;
+        curr = &list[co_so_nganh].head;
+        for(int i = 1; i < size; i++){
+            temp = Allocate_Pool(
+                (Subject_Payload){
+                    .term = arr[i].x,
+                    .credit_hour = arr[i].y,
+                    .ratio = arr[i].z,
+                    .link = NULL
+                }
+            );
+            strncpy(temp->SUBJECT_ID,ID[i],SUBJECT_ID_SIZE);
+            strncpy(temp->SUBJECT_NAME,name[i],SUBJECT_NAME_SIZE);
+
+            curr->link = temp;
+            curr = curr->link;
+        }
     }
+
+    // write all data to file
+    int32_t magic_b = 0x12345678; // using for check endainness
+    fwrite(&magic_b,1,4,file);
+    fwrite(&data.capacity_type_subject,1,2,file);
+    Subject_Payload* curr = NULL;
+    bype temp_b = 0x00;
+    for(int i = 0; i < data.capacity_type_subject; i++){
+        fwrite(&data.list_type_subject[i].Subject_Type_Name,1,SUBJECT_NAME_SIZE,file);
+        curr = &data.list_type_subject[i].head;
+        if(curr->term == 0){
+            temp_b = 0x00;
+            fwrite(&temp_b,1,1,file);
+            continue;
+        }
+        temp_b = 0x0f;
+        while(curr){
+            fwrite(&temp_b,1,1,file);
+            fwrite(&curr->term,1,1,file);
+            fwrite(&curr->credit_hour,1,1,file);
+            fwrite(&curr->ratio,1,1,file);
+            fwrite(curr->SUBJECT_ID,1,SUBJECT_ID_SIZE,file);
+            fwrite(curr->SUBJECT_NAME,1,SUBJECT_NAME_SIZE,file);
+            curr = curr->link;
+        }
+        temp_b = 0x00;
+        fwrite(&temp_b,1,1,file);
+    }
+
+    // Free Pool
+    Detroy_Pool();
+
+    // File close
+    fclose(file);
+
+    printf("[FUNC] Completed Write File\n");
 }
